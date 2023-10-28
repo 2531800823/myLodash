9c45fcc777068110512d1a06f44384a936004eef
刘士朋 <2531800823@qq.com>
2023-10-28 23:26:41 +0800

    slice

---
+++ src/Array/slice.ts
@@ -0,0 +1,28 @@
+/** 裁剪数组array，从 start 位置开始到end结束，但不包括 end 本身的位置。 */
+function slice<T>(array: T[], start: number, end: number) {
+  let length = array == null ? 0 : array.length;
+  if (!length) {
+    return [];
+  }
+  start = start == null ? 0 : start;
+  end = start == undefined ? length : end;
+
+  if (start < 0) {
+    start = -start > length ? 0 : length + start;
+  }
+  end = end > length ? length : end;
+  if (end < 0) {
+    end += length;
+  }
+
+  length = start > end ? 0 : (end - start) >>> 0;
+  start >>>= 0;
+
+  let index = -1;
+  const result = new Array(length);
+  while (++index < length) {
+    result[index] = array[index + start];
+  }
+  return result;
+}
+export default slice;
