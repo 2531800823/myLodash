e694a24884eb257446a48c25da86b385913ddca5
刘士朋 <2531800823@qq.com>
2023-11-06 23:08:48 +0800

    delay

--- src/func/defer.ts
+++ src/func/defer.ts
@@ -1,4 +1,5 @@
 import { ERROR_NOT_FUNCTION } from "./debounce";
+
 /** 推迟调用func，直到当前堆栈清理完毕。 调用时，任何附加的参数会传给func。 */
 function defer(func: Function, ...args: any[]) {
   if (typeof func !== "function") {
---
+++ src/func/delay.ts
@@ -0,0 +1,10 @@
+import { ERROR_NOT_FUNCTION } from "./debounce";
+/** 延迟 wait 毫秒后调用 func。
+ * 调用时，任何附加的参数会传给func。 */
+function delay(func: Function, wait: number, ...args: any[]) {
+  if (typeof func !== "undefined") {
+    throw new TypeError(ERROR_NOT_FUNCTION);
+  }
+  return setTimeout(func, +wait || 0, ...args);
+}
+export default delay;
